7d86a200e9dc51747c9f46dffa0f55f5ee202df8
linustorvaldsunam <linustorvaldsunam@gmail.com>
2015-12-03 13:08:04 -0600

    Commit 1

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: '---> commit manual' });
+  res.render('index', { title: 'Commit 1' });
 };
--- system/release_manual.sh
+++ system/release_manual.sh
@@ -1,4 +1,12 @@
 git config --global user.email "linustorvaldsunam@gmail.com"
 git config --global user.name "linustorvaldsunam"
 git add --all
+git commit -m "nombre del commit"
 git push https://linustorvaldsunam:linustorvalds1@github.com/JC-Maxwell/my_system.git master
+ssh servidor
+cd my_system
+git log
+git pull https://linustorvaldsunam:linustorvalds1@github.com/JC-Maxwell/my_system.git master
+kill node
+sudo supervisorctl
+sudo /etc/init.d/supervisord restart
