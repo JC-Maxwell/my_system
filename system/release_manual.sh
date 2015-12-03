a11eb82474765c4af86deae8b40c03e9bed94503
linustorvaldsunam <linustorvaldsunam@gmail.com>
2015-12-03 13:16:15 -0600

    Commit 2

--- system/release_manual.sh
+++ system/release_manual.sh
@@ -1,3 +1,8 @@
+1. Describir el problema
+2. Qué es git?
+3. Qué es github?
+4. Resolverlo a mano (.ssh, .pem, config, etc)
+
 git config --global user.email "linustorvaldsunam@gmail.com"
 git config --global user.name "linustorvaldsunam"
 git add --all
@@ -10,3 +15,5 @@ git pull https://linustorvaldsunam:linustorvalds1@github.com/JC-Maxwell/my_syste
 kill node
 sudo supervisorctl
 sudo /etc/init.d/supervisord restart
+
+5. Mostrarles el script (.ssh, .pem, config, etc)
